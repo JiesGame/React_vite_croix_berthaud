ac28512ec3087706f3adfbb08a9a46430c511074
NicolasVdev <nicolasvadelorge.dev@gmail.com>
2023-09-12 18:03:29 +0200

    indentation ok svg fb à revoir + class en classname

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,23 +12,24 @@ export const Footer = () => {
       <div className="grid grid-cols-3 py-3 mx-6">
         <div className="flex justify-start">
           <div className="grid">
-          <p className="font-semibold">Maison de quartier</p>
-          <p className="font-semibold">La Croix Berthaud</p>
-          <hr className="my-1"/>
-          <p>Square Henri Dunant</p>
-          <p>42400 Saint-Chamond</p>
+            <p className="font-semibold">Maison de quartier</p>
+            <p className="font-semibold">La Croix Berthaud</p>
+            <hr className="my-1"/>
+            <p>Square Henri Dunant</p>
+            <p>42400 Saint-Chamond</p>
           </div>
         </div>
         <div className="flex justify-center">
           <ul className="flex place-items-end">
             <li>
-            <a href="https://www.facebook.com/lacroixberthaud/?locale=fr_FR" target="_blank" rel="noopener noreferrer" class="text-white dark w-fit">
-            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                <path fill-rule="evenodd"
+              <a href="https://www.facebook.com/lacroixberthaud/?locale=fr_FR" target="_blank" rel="noopener noreferrer" className="text-white dark w-fit">
+                <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                  <path fill-rule="evenodd"
                     d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-                    clip-rule="evenodd"></path>
-            </svg>
-          </a>
+                    clip-rule="evenodd"> 
+                  </path>
+                </svg>
+              </a>
             </li>
             <li className="mx-2">|</li>
             <li>
