c3e61137d4ff16a7c254adee20de5b9d426642e1
NicolasVdev <nicolasvadelorge.dev@gmail.com>
2023-09-13 21:38:52 +0200

    add card article responsive ok mais à retoucher

--- src/components/ListArticles.jsx
+++ src/components/ListArticles.jsx
@@ -14,13 +14,17 @@ export const ListArticles = () => {
     <div className="font-semibold">
       <h1 className="text-center">Dernières actualités</h1>
       {dataArticles.map((article) => (
+        <>
+        {console.log(typeof article.created_at)}
         <Article
         key={article.id}
         id={article.id}
         title={article.title}
         content={article.content}
         isLinkVisible={true}
-      />    
+        created_at={article.created_at}
+      /> 
+      </>   
       ))}
     </div>
   )
