4471a464e81b845d42073015f6b556d17431148e
NicolasVdev <nicolasvadelorge.dev@gmail.com>
2023-09-13 10:38:17 +0200

    fixing layout, responsive footer, FR 404

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ export const Footer = () => {
 
   if (!isNotFoundPage) {
     return ( 
-    <footer className="primary-bg text-white w-full absolute bottom-0">
+    <footer className="primary-bg text-white w-full">
       <div className="grid grid-cols-3 py-3 mx-6">
         <div className="flex justify-start">
           <div className="grid">
@@ -20,9 +20,9 @@ export const Footer = () => {
           </div>
         </div>
         <div className="flex justify-center">
-          <ul className="flex place-items-end">
+          <ul className="md:flex md:place-items-end grid place-items-stretch text-center">
             <li>
-              <a href="https://www.facebook.com/lacroixberthaud/?locale=fr_FR" target="_blank" rel="noopener noreferrer" className="text-white dark w-fit">
+              <a href="https://www.facebook.com/lacroixberthaud/?locale=fr_FR" target="_blank" rel="noopener noreferrer" className="text-white dark flex justify-center">
                 <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                   <path fill-rule="evenodd"
                     d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
@@ -31,11 +31,11 @@ export const Footer = () => {
                 </svg>
               </a>
             </li>
-            <li className="mx-2">|</li>
+            <li className="md:mx-2 hidden md:inline-block">|</li>
             <li>
               <a href="#" className="text-white hover:underline font-semibold">Contact</a>
             </li>
-            <li className="mx-2">|</li>
+            <li className="md:mx-2 hidden md:inline-block">|</li>
             <li>
               <a href="#" className="text-white hover:underline font-semibold">Mentions légales</a>
             </li>
