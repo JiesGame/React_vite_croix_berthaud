96286d74e4c3be59258c213fa8eda906b0fc33b1
NicolasVdev <nicolasvadelorge.dev@gmail.com>
2023-09-13 15:57:33 +0200

    login form ok

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,7 +10,7 @@ import { userAtom } from '../store/atoms';
 export const Login = () => {
   const schema = yup.object().shape({
     email: yup.string().required("L'email est requis."),
-    password: yup.string().required()
+    password: yup.string().required("Le mot de passe est requis.")
   });
 
   const {register, handleSubmit, formState: {errors} } = useForm({
@@ -32,41 +32,50 @@ export const Login = () => {
   }
 
   return (
-    <div className='flex items-center justify-center '>
-      <div className="w-[26rem]">
-        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit(onSubmit)}>
+    <div className='flex items-center justify-center my-14'>
+      <div className="w-[30rem] h-fit">
+        <form className="bg-white shadow-md rounded h-full" onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-4">
-            <h1 className='text-2xl text-center mb-4'>Connexion</h1>
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              Email
-            </label>
-            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" type="text" placeholder="Email..." {...register('email')} />
-            {errors.email?.message && <p className="text-red-500 text-xs">{errors.email?.message}</p>}
+            <h1 className='flex text-2xl justify-center items-center primary-bg w-full text-white h-12 rounded-t font-semibold'>Se connecter</h1>
+            <div className='px-10 mt-4'>
+              <label className="block primary text-xl font-semibold mb-2">
+                Email
+              </label>
+              <input className="bg-white appearance-none border-2 light-gray-border rounded-lg w-full py-2 px-4 primary leading-tight focus:outline-none focus:bg-white focus:ring-0 shadow-lg h-12 placeholder-gray-300 font-medium" type="text" placeholder="exemple@email.fr" {...register('email')} />
+              {errors.email?.message && <p className="ml-1 pt-2 font-semibold text-red-500 text-sm">{errors.email?.message}</p>}
+            </div>
           </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              Mot de passe
-            </label>
-            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="password" placeholder="Mot de passe..." {...register('password')} />
-            {errors.password?.message && <p className="text-red-500 text-xs">{errors.password?.message}</p>}
+          <div className="mt-8">
+            <div className='px-10'>
+              <label className="block primary text-xl font-semibold mb-2">
+                Mot de passe
+              </label>
+              <input className="bg-white appearance-none border-2 light-gray-border rounded-lg w-full py-2 px-4 primary leading-tight focus:outline-none focus:bg-white focus:ring-0 shadow-lg h-12 placeholder-gray-300 font-medium" type="password" placeholder="Mot de passe" {...register('password')} />
+              {errors.password?.message && <p className="ml-1 pt-2 font-semibold text-red-500 text-sm">{errors.password?.message}</p>}
+            </div>
           </div>
-          <div className="flex justify-around mb-4">
-            <input type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" value="Se connecter" />
+          <div className="flex items-center justify-center my-8">
+            <input id="default-checkbox" type="checkbox" value="" className="w-4 h-4"/>
+            <label for="default-checkbox" className="ml-2 text-sm font-medium primary">J'accepte les <a href="#" className='hover:underline font-bold'>Conditions Générales d'Utilisation</a></label>
           </div>
-          <div>
-            <p className='mb-2'>
-            <Link to="/register" className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">
-              Créer un compte
-            </Link>
-            </p>
-            <Link to="/forgot_password" className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">
+          <div className="flex justify-around items-center mb-8 mt-6">
+            <input type="submit" className="primary-bg text-white font-bold py-2 px-4 w-32 rounded focus:outline-none focus:shadow-outline" value="Connexion" />
+            <Link to="/forgot_password" className="font-medium text-md primary hover:underline">
               Mot de passe oublié ?
             </Link>
           </div>
+          <div>
+            <div className='px-6 mb-2'>
+              <hr  className='light-gray-border border-[1px]'/>
+            </div>
+            <div className='mb-10 flex gap-2 justify-center'>
+              <p className='font-small text-md primary mb-4'>Pas encore adhérent ?</p>
+            <Link to="/register" className="text-center flex justify-center font-medium text-md primary hover:underline">
+              Créer un compte
+            </Link>
+            </div>
+          </div>
         </form>
-        <p className="text-center text-gray-500 text-xs">
-          &copy;2023 JiesGame. All rights reserved.
-        </p>
       </div>
     </div>
   )
