6e254253ce9f2d651c0cc3de46645d77b1272617
NicolasVdev <nicolasvadelorge.dev@gmail.com>
2023-09-19 15:42:10 +0200

    form AdminCreateArticle ok

--- src/pages/admin/AdminCreateArticle.jsx
+++ src/pages/admin/AdminCreateArticle.jsx
@@ -31,10 +31,17 @@ export const AdminCreateArticle = () => {
   }
 
   return (
-    <form onSubmit={onSubmit}>
-      <div>
-        <label htmlFor="title">Titre :</label>
+    <div className="flex items-center justify-center my-14">
+      <div className="w-[30rem] h-fit">
+    <form className="bg-white shadow-lg rounded h-full" onSubmit={onSubmit}>
+    <div className="mt-8">
+            <h1 className="flex text-2xl justify-center items-center primary-bg w-full text-white h-12 rounded-t font-semibold">
+              Création d'un article
+            </h1>
+      <div className="px-10 mt-4">
+        <label className="block primary text-xl font-semibold mb-2" htmlFor="title">Titre :</label>
         <input
+          className='bg-white appearance-none border-2 light-gray-border rounded-lg w-full py-2 px-4 primary leading-tight focus:outline-none focus:bg-white focus:ring-0 shadow-lg h-12 placeholder-gray-300 font-medium dark'
           type="text"
           id="title"
           name="title"
@@ -42,18 +49,19 @@ export const AdminCreateArticle = () => {
           onChange={handleChange}
         />
       </div>
-      <div>
-        <label htmlFor="content">Contenu :</label>
+      <div className='px-10 mt-4'>
+        <label className="block primary text-xl font-semibold mb-2" htmlFor="content">Contenu :</label>
         <textarea
+          className='bg-white appearance-none border-2 light-gray-border rounded-lg w-full py-2 px-4 primary leading-tight focus:outline-none focus:bg-white focus:ring-0 shadow-lg h-36 placeholder-gray-300 font-medium dark'
           id="content"
           name="content"
           value={dataArticle.content}
           onChange={handleChange}
         />
       </div>
-      <div>
-        <label htmlFor="category">Catégorie :</label>
-      <select id="category" name="category" value={dataArticle.category} onChange={handleChange}>
+      <div className='flex gap-2 px-10 mt-4'>
+        <label className="block primary text-xl font-semibold mb-2" htmlFor="category">Catégorie :</label>
+      <select className="pb-1 border-[1px] shadow-md" id="category" name="category" value={dataArticle.category} onChange={handleChange}>
         <option value="news">News</option>
         <option value="maison_de_quartier">Maison de quartier</option>
         <option value="programme">Programme</option>
@@ -62,17 +70,25 @@ export const AdminCreateArticle = () => {
         <option value="activites_adultes">Activités adultes</option>
       </select>
       </div>
-      <div>
-        <label htmlFor="private">Privé :</label>
+      <div className='flex gap-2 px-10 mt-4'>
+        <label className='block primary text-xl font-semibold mb-2' htmlFor="private">Privé :</label>
+        <div className='flex justify-center items-center pb-1'>
         <input
+          className='w-[18px] h-[20px]'
           type="checkbox"
           id="private"
           name="private"
           checked={dataArticle.private}
           onChange={handleChange}
         />
+        </div>
       </div>
-      <button type="submit">Envoyer</button>
+      <div className='flex justify-center mt-8'>
+      <button className="button primary-bg text-white font-bold py-2 px-4 w-32 rounded focus:outline-none focus:shadow-outline cursor-pointer mb-10" type="submit">Envoyer</button>
+      </div>
+      </div>
     </form>
+    </div>
+    </div>
   );
 };
