367abebfcbb9fd86d157593aa8a318f33060c897
jace <nic.chiron@gmail.com>
2023-09-22 12:30:01 +0200

    stripe for inscription

--- src/services/axiosArticle.js
+++ src/services/axiosArticle.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-// const baseURL = "https://api-croix-berthaud-0572b1b3d9d4.herokuapp.com"
 const baseURL = import.meta.env.VITE_API_URL;
 
 export const articlesFetch = async (setDataArticles) => {
