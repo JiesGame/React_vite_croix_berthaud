64e2ddc0e858016d2106b12ef645ca59e82cdbb4
vincent <kratos_angel_59@hotmail.fr>
2023-09-24 15:51:52 +0200

    edit responsivity on few pages

--- src/components/admin/AdminPanel.jsx
+++ src/components/admin/AdminPanel.jsx
@@ -5,18 +5,18 @@ export const AdminPanel = () => {
   
   return (
     <article className='text-center flex 2xl:fixed justify-center'>
-      <div className="px-5 2xl:w-[300px] sm:w-[500px] w-auto">
+      <div className="px-5 2xl:w-[250px] sm:w-[600px] w-auto">
         <div className="dark-bg navbar white border-2 rounded-lg shadow-lg w-full my-10">
           <div className="primary-bg rounded-t py-3">
-            <p className="text-white font-bold">
+            <p className="text-white font-bold px-2">
               Page d'administration des :
             </p>
           </div>
-          <div className='2xl:py-0 py-2 2xl:mx-0 sm:mx-4'>
-            <Link to='/admin_articles' className='font-semibold 2xl:block 2xp:px-0 sm:px-6 px-3'>Articles</Link>
-            <Link to='/admin_users' className='font-semibold 2xl:block 2xp:px-0 sm:px-6 px-3'>Utilisateurs</Link>
-            <Link to='/admin_activities' className='font-semibold 2xl:block 2xp:px-0 sm:px-6 px-3'>Activités</Link>
-            <Link to='/admin_inscriptions' className='font-semibold 2xl:block 2xp:px-0 sm:px-6 px-3'>Inscriptions</Link>
+          <div className='2xl:py-0 sm:py-2 2xl:mx-0 sm:mx-4 grid sm:grid-cols-1 grid-cols-2'>
+            <Link to='/admin_articles' className='font-semibold 2xl:block 2xp:px-0 sm:px-6 px-1'>Articles</Link>
+            <Link to='/admin_users' className='font-semibold 2xl:block 2xp:px-0 sm:px-6 px-1'>Utilisateurs</Link>
+            <Link to='/admin_activities' className='font-semibold 2xl:block 2xp:px-0 sm:px-6 px-1'>Activités</Link>
+            <Link to='/admin_inscriptions' className='font-semibold 2xl:block 2xp:px-0 sm:px-6 px-1'>Inscriptions</Link>
           </div>
           <div className="flex md:justify-between md:flex-row md:items-center primary-bg text-white rounded-b py-1 flex-col items-center" />
         </div>
--- src/pages/admin/AdminInscriptions.jsx
+++ src/pages/admin/AdminInscriptions.jsx
@@ -24,55 +24,60 @@ export const AdminInscriptions = () => {
   return (
     <>
       <AdminPanel />
-      {console.log(usersData)}
       <h1 className="text-4xl primary mt-5 text-center">Gestion des inscriptions</h1>
       <article className='w-full'>
-        {usersData.map((user) => (
-          <div key={user.id}>
-            <h2 className="text-2xl primary mt-5 text-center">{user.email}</h2>
-            <div className="mx-auto sm:w-[700px] rounded-lg shadow-lg my-5 primary-bg">
-              <table className="w-full my-3">
-                <thead>
-                  <tr className="text-white text-center">
-                    <th className="pb-3">Membres</th>
-                    <th className="pb-3">Activités</th>
-                    <th className="pb-3">Validation</th>
-                  </tr>
-                </thead>
-                <tbody className='white-bg'>
-                  {user.family_members.map((family_member) => (
-                    <tr key={family_member.id} className="text-center">                
-                      <td className="">
-                        {family_member.firstname} {family_member.lastname}
-                      </td>
-                      <td className="">
-                        {family_member.activities.map((activity, index) => (
-                          <div key={activity.id}>
-                            {!family_member.family_member_activities[index].validation &&
-                              <p>{activity.name}</p>
-                            }
-                          </div>
-                        ))}
-                      </td>
-                      <td>
-                        {family_member.family_member_activities.map((activity, index) => (
-                          <div key={index}>
-                            {!activity.validation &&
-                              <>
-                                <button className="primary-bg text-white font-bold py-2 sm:px-4 px-2 rounded focus:outline-none focus:shadow-outline"onClick={() => handleValidateClick(activity.id)}>Oui</button>
-                                <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 sm:px-4 px-2 rounded focus:outline-none focus:shadow-outline" onClick={() => handleDeleteClick(activity.id)}>Non</button>
-                              </>
-                            }                  
-                          </div>
-                        ))}
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
-        ))}
+        {usersData.length < 1 ? (
+          <h2 className="text-2xl primary my-5 text-center">Aucune inscription en cours</h2>
+        ) : (
+          <>
+            {usersData.map((user) => (
+              <div key={user.id}>
+                <h2 className="text-2xl primary mt-5 text-center">{user.email}</h2>
+                <div className="mx-auto sm:w-[700px] rounded-lg shadow-lg my-5 primary-bg">
+                  <table className="w-full my-3">
+                    <thead>
+                      <tr className="text-white text-center">
+                        <th className="pb-3">Membres</th>
+                        <th className="pb-3">Activités</th>
+                        <th className="pb-3">Validation</th>
+                      </tr>
+                    </thead>
+                    <tbody className='white-bg'>
+                      {user.family_members.map((family_member) => (
+                        <tr key={family_member.id} className="text-center">                
+                          <td className="">
+                            {family_member.firstname} {family_member.lastname}
+                          </td>
+                          <td className="">
+                            {family_member.activities.map((activity, index) => (
+                              <div key={activity.id}>
+                                {!family_member.family_member_activities[index].validation &&
+                                  <p>{activity.name}</p>
+                                }
+                              </div>
+                            ))}
+                          </td>
+                          <td>
+                            {family_member.family_member_activities.map((activity, index) => (
+                              <div key={index}>
+                                {!activity.validation &&
+                                  <>
+                                    <button className="primary-bg text-white font-bold py-2 sm:px-4 px-2 rounded focus:outline-none focus:shadow-outline"onClick={() => handleValidateClick(activity.id)}>Oui</button>
+                                    <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 sm:px-4 px-2 rounded focus:outline-none focus:shadow-outline" onClick={() => handleDeleteClick(activity.id)}>Non</button>
+                                  </>
+                                }                  
+                              </div>
+                            ))}
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            ))}
+          </>
+        )}
         {isDeleteConfirmation && (
           <div className="fixed inset-0 flex items-center justify-center z-50">
             <div className="dark-bg white p-4 rounded shadow-md">
